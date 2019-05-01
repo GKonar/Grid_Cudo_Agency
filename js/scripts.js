cb16a7732f9562a4c2a50685125ee8b51fc2f908
Grzegorz Konarski <grzegorz.konarski86@gmail.com>
2019-05-01 13:49:25 +0200

    Responsive added to services

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,13 @@
 // VARS
 const mobileNavBtn = document.querySelector('.nav__mobile-btn');
 const mobileNavItems = document.querySelector('.nav__mobile-items');
+const mobileNavItem = document.querySelectorAll('.nav__mobile-items--item');
 
 // LISTENERS
-window.addEventListener('scroll', (e) => {
-    mobileNavItems.style.visibility = "hidden";
-});
+window.addEventListener('scroll', (e) => mobileNavItems.style.visibility = "hidden");
 
-mobileNavBtn.addEventListener('click', (e) => {
-    mobileNavItems.style.visibility = "visible";
+mobileNavBtn.addEventListener('click', (e) => mobileNavItems.style.visibility = "visible");
+
+mobileNavItem.forEach(item => {
+    item.addEventListener('click', e =>  alert('No path added yet :) Scroll to exit nav'));
 });
